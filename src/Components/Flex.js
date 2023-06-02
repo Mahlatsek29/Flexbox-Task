8913e65751b8aee02cd0f5f0555e9b647ee50901
Mahlatsek29 <124674856+Mahlatsek29@users.noreply.github.com>
2023-06-02 08:33:36 -0700

    fisrt commit

---
+++ src/Components/Flex.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Flex.css";
+
+const Flex = () => {
+return (
+<div className="App">
+    <div className="flex-container">
+        <div className="flexbox-item flexbox-item-1"></div>
+        <div className="flexbox-item flexbox-item-2"></div>
+        <div className="flexbox-item flexbox-item-3"></div>
+        <div className="flexbox-item flexbox-item-4"></div>
+        <div className="flexbox-item flexbox-item-5"></div>
+        <div className="flexbox-item flexbox-item-6"></div>
+        <div className="flexbox-item flexbox-item-7"></div>
+
+    </div>
+    
+</div>
+);
+};
+
+export default Flex;
